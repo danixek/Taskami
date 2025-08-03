da3953697c031545bcf6494119fb71222bd4b8c5
danixek <danielkefurt@gmail.com>
2025-08-03 05:12:35 +0200

    Add project files.

---
+++ Taskami.WPFUI/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Taskami.Views;
+using System.Windows;
+
+namespace Taskami
+{
+    /// <summary>
+    /// Interaction logic for Taskami.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowTodoView(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TodoView();
+        }
+
+        private void ShowDiaryView(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new DiaryView();
+        }
+
+        private void ShowPomodoroView(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new PomodoroView();
+        }
+    }
+}
---
+++ Taskami.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taskami.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
