5431d92515fffe00d60db89f9d95c09b6e16dc78
danixek <danielkefurt@gmail.com>
2025-08-04 09:24:14 +0200

    Added Layout of WebUI

--- Taskami.WebUI/Controllers/HomeController.cs
+++ Taskami.WebUI/Controllers/HomeController.cs
@@ -6,21 +6,42 @@ namespace TaskamiUI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        // The main landing page of the application
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        // The main dashboard where users can see their tasks and activities
+        public IActionResult Today()
         {
             return View();
+        }
+        // The page where users can view and manage their tasks - calendar mode
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+        // Filters and labels are used to categorize tasks
+        public IActionResult Filters()
+        {
+            return View();
+        }
+        // Pomodoro is a time management technique that uses a timer to break work into intervals
+        public IActionResult Pomodoro()
+        {
+            return View();
+        }
+        // The settings page where users can configure their preferences
+        public IActionResult Settings()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Reschedule()
+        {
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
